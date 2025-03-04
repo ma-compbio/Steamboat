4993d67a180f168174dad426bd23a216698757ad
lshh125 <shaohengliang@gmail.com>
2025-03-04 15:19:51 -0500

    update tutorials

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2024, Guanghan Wang, Shaoheng Liang, Jian Ma'
 author = 'Guanghan Wang, Shaoheng Liang, Jian Ma'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.1.1'
 
 
 # -- General configuration ---------------------------------------------------
