5068222df7b45daaa6669b970cb7513d8b05063c
lshh125 <shaohengliang@gmail.com>
2025-12-10 03:09:54 -0500

    Update tests

---
+++ tests/integrated_test.py
@@ -0,0 +1,6 @@
+import sys
+sys.path.append('C:/Files/projects/Steamboat/')
+import steamboat as sf # "sf = steamboat factorization"
+
+def test_dummy():
+    pass
--- tests/test_integrated.py
+++
@@ -1,6 +0,0 @@
-# import sys
-# sys.path.append('C:/Files/projects/Steamboat/')
-import steamboat as sf # "sf = steamboat factorization"
-
-def dummy():
-    pass
