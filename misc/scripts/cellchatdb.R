95ef0e5c265c8e59a7ec915869c9818ef473e7fa
lshh125 <shaohengliang@gmail.com>
2025-12-11 17:27:50 -0500

    rearrange cellchatdb

--- misc/scripts/cellchatdb.R
+++ misc/scripts/cellchatdb.R
@@ -1,2 +1,5 @@
 load("C:/Files/data/CellChat/CellChatDB.human.rda")
 write.csv(CellChatDB.human$interaction, file="C:/Files/data/CellChat/CellChatDB_human_interaction.csv", row.names=FALSE)
+
+load("C:/Files/data/CellChat/CellChatDB.mouse.rda")
+write.csv(CellChatDB.mouse$interaction, file="C:/Files/data/CellChat/CellChatDB_mouse_interaction.csv", row.names=FALSE)
